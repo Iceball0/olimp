a91f3f6af63bd99d12d53bcdc1c73cc88bc0935a
Пользователь2 <email>
2021-01-16 09:33:25 +0300

    111

--- main2.py
+++ main2.py
@@ -1,12 +1,11 @@
-import math
-
-s = int(input())
-t = int(input())
-li = []
-for i in range(t):
-    li.append(int(input()))
-
-if s == 0:
-    print(max(li))
+k = int(input())
+a, x = map(int, input().split())
+b, y = map(int, input().split())
+if x == 0 and a == 0 and (k - b) >= 0:
+    print((k - b) * y)
+elif a == 0 and b == 0:
+    print(k * max(x, y))
+elif a == b and (k - a) >= 0:
+    print((k - a) * max(x, y))
 else:
-    print(math.ceil((s + sum(li)) / t))
+    print(0)
