894c77d888ba23018f08ab9659d1bbbf97ab2f0e
Пользователь2 <email>
2021-01-16 11:54:36 +0300

    111

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-li = [3, 4, 5, 6]
-print(li[(0 - 2) % len(li):] + li[:(0 - 2) % len(li)])
+k = int(input())
+a, x = map(int, input().split())
+b, y = map(int, input().split())
+
+if (k - a) * x >= (k - b) * y:
+    if (k - a - b) >= 0:
+        print((k - a) * x + (k - a - b) * y)
+    elif (k - a) >= 0:
+        print((k - a) * x)
+    else:
+        print(0)
+elif (k - a) * x < (k - b) * y:
+    if (k - a - b) >= 0:
+        print((k - a - b) * x + (k - b) * y)
+    elif (k - b) >= 0:
+        print((k - b) * y)
+    elif (k - a) >= 0:
+        print((k - a) * x)
+    else:
+        print(0)
+else:
+    print(0)
