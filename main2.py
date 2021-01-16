71c72ebaa9f15a168aa0d1d5c2343fb026af97f5
Пользователь2 <email>
2021-01-16 11:10:35 +0300

    111

--- main2.py
+++ main2.py
@@ -1,12 +1,15 @@
 k = int(input())
 a, x = map(int, input().split())
 b, y = map(int, input().split())
-if x == 0 and a == 0 and (k - b) >= 0:
-    print((k - b) * y)
-elif a == 0 and b == 0:
-    print(k * max(x, y))
-elif a == b and (k - a) >= 0:
-    print((k - a) * max(x, y))
-
+if (k - a) * x >= (k - b) * y:
+    if (k - a - b) >= 0:
+        print((k - a) * x + (k - a - b) * y)
+    elif (k - a) >= 0:
+        print((k - a) * x)
+elif (k - a) * x < (k - b) * y:
+    if (k - a - b) >= 0:
+        print((k - a - b) * x + (k - b) * y)
+    elif (k - b) >= 0:
+        print((k - b) * y)
 else:
     print(0)
