caa56ea486ad828c74fcda0b54392bc339208041
Пользователь2 <email>
2021-01-18 11:51:13 +0300

    111

--- main2.py
+++ main2.py
@@ -1,8 +1,32 @@
 x = int(input())
 k = int(input())
+
+
+def check(lister):
+    lister = list(map(int, list(str(lister))))
+    c = max(lister)
+    d = min(lister)
+    b = lister.count(c)
+    d = lister.count(d)
+    if b == len(lister) - 1 and d == 1:
+        return True
+    elif b == 1 and d == len(lister) - 1:
+        return True
+    return False
+
+
 if k == 0:
-    i = str(x)[0]
-    res = int(i * len(str(x)))
-    if res < x:
-        res = int(str(int(i) + 1) * len(str(x)))
-    print(res)
+    elem = str(x)[0]
+    total = int(elem * len(str(x)))
+    if total < x:
+        total = int(str(int(elem) + 1) * len(str(x)))
+    print(total)
+if k == 1:
+    lst = list(map(int, list(str(x))))
+    if len(str(x)) > 1:
+        for i in range(x + 10 ** 17):
+            if check(x + i):
+                print(x + i)
+                break
+    else:
+        print(x)
